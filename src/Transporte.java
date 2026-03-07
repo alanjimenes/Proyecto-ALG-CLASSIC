fdd3d89166a6d49e47fb1121a604c0d80a89ed01
Coffee <alanjimenes2006@gmail.com>
2026-03-07 11:45:35 -0400

    Creacion de la clase transporte faltando el editRuta

---
+++ src/Transporte.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.*;
+
+public class Transporte {
+    private Map<String, Parada> paradaMap;
+    private Map<String, List<Ruta>> listaAdyacencia;
+
+    public Transporte() {
+        this.paradaMap = new HashMap<>();
+        this.listaAdyacencia = new HashMap<>();
+    }
+
+    public void addParada(String id, String nombre) {
+        if (!paradaMap.containsKey(id)) {
+            Parada new_parada = new Parada(id, nombre);
+            paradaMap.put(id, new_parada);
+            listaAdyacencia.put(id, new ArrayList<>());
+        }
+    }
+
+    public void editParada(String id, String nombre) {
+        if (paradaMap.containsKey(id)) {
+            Parada p = paradaMap.get(id);
+            p.setNombre(nombre);
+        } else {
+            System.out.println("Error: La parada con ID " + id + " no existe.");
+        }
+    }
+    public void deleteParada(String id) {
+        if (paradaMap.containsKey(id)) {
+            paradaMap.remove(id);
+            listaAdyacencia.remove(id);
+            for (List<Ruta> rutas : listaAdyacencia.values()) {
+                rutas.removeIf(ruta -> ruta.getDestino().getId().equals(id));
+            }
+        }
+    }
+
+    public void addRuta(String id_Origin, String id_Destination, double tiempo, double distancia, double costo, boolean trasbordo) {
+        if (paradaMap.containsKey(id_Origin) && paradaMap.containsKey(id_Destination)) {
+            Parada destino = paradaMap.get(id_Destination);
+            listaAdyacencia.get(id_Origin).add(new Ruta(destino, tiempo, distancia, costo, trasbordo));
+        } else {
+            System.err.println("No existe un origen o un destino mi rey/reina");
+
+        }
+    }
+
+
+
+    public void deleteRuta(String id_Origin, String id_Destination) {
+        if (listaAdyacencia.containsKey(id_Destination)) {
+            List<Ruta> rutas = listaAdyacencia.get(id_Origin);
+            rutas.removeIf(ruta -> ruta.getDestino().getId().equals(id_Destination));
+        }
+    }
+}
