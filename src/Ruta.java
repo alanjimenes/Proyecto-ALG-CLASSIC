c1f20fac05fc065fa63a0044de09e5520fe5bf9f
Coffee <alanjimenes2006@gmail.com>
2026-03-07 10:49:03 -0400

    Creacion de la clase ruta

---
+++ src/Ruta.java
@@ -0,0 +1,47 @@
+public class Ruta {
+    private Parada destino;
+    private double tiempoMinuto;
+    private double distanciaKm;
+    private double costo;
+    private boolean requiereTrasbordo;
+
+    public Ruta(Parada destino, double tiempoMinuto, double distanciaKm, double costo, boolean requiereTrasbordo) {
+        this.destino = destino;
+        this.tiempoMinuto = tiempoMinuto;
+        this.distanciaKm = distanciaKm;
+        this.costo = costo;
+        this.requiereTrasbordo = requiereTrasbordo;
+    }
+    public Parada getDestino(){
+        return destino;
+    }
+    public double getTiempoMinuto()
+    {
+        return tiempoMinuto;
+    }
+
+    public double getDistanciaKm() {
+        return distanciaKm;
+    }
+
+    public void setDistanciaKm(double distanciaKm) {
+        this.distanciaKm = distanciaKm;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public boolean isRequiereTrasbordo() {
+        return requiereTrasbordo;
+    }
+
+    public void setRequiereTrasbordo(boolean requiereTrasbordo) {
+        this.requiereTrasbordo = requiereTrasbordo;
+    }
+
+}
